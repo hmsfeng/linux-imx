2245259ec93060b86cd2055a136d63af9aeebcc1
hmsfeng <hmsfeng@163.com>
2020-02-21 19:56:31 +0800

    添加配置和设备树

--- build_imx6ull.sh
+++ build_imx6ull.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- imx_v7_defconfig
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j8
+#make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
+#make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- imx6ull_zdyz_emmc_defconfig
+#make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
+#make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j8
+make -j8
+cp -rf ./arch/arm/boot/zImage ~/tftpboot
+cp -rf ./arch/arm/boot/dts/imx6ull-zdyz-emmc-7-800*480.dtb ~/tftpboot
+echo "copy finish"
